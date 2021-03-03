c99acfd2831ce80a2a7267e73a7d6d79c066934f
unknown <shireenkk28@gmail.com>
2021-03-03 23:56:36 +0530

    [shireen]added length of the line

---
+++ LineComparison/LineComparison/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LineComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = 2;
+            int x2 = 3;
+            int y1 = 2;
+            int y2 = 6;
+            int length = 0;
+            length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("length is: " + length);
+            Console.ReadKey();
+        }
+    }
+}
